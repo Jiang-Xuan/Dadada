857a6175636948846dd94c3d9dfb92f2e578eeb1
jiangxuan <jiangxuan@yuantutech.com>
2017-03-11 00:34:25 +0800

    save

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strict'
+const path = require('path')
+const config = {
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
+
+module.exports = config
